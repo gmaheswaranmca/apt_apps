7d80fccea2fd2c2cd6ba4ec5a0730967d57a5bb7
Maheswaran <pystud19@gmail.com>
2024-02-03 17:38:10 +0530

    done

---
+++ 00-original/files/runMe.bat
@@ -0,0 +1,7 @@
+@echo off
+cmd /k "cd /d F:\v2m\techLang\dev\php\apt\aaa\aaapydj\app & envl\Scripts\activate & cd /d F:\v2m\techLang\dev\php\apt\aaa\aaapydj\app\aptmktest & start http://localhost/apt/aaa/qnpro/fev5 & python manage.py runserver & pause"
+
+
+
+
+
